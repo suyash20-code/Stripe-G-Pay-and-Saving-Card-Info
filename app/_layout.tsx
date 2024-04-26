574c63b8ba61a77982ee14294ac25017559dd93a
suyash20-code <suyashpadwal10@gmail.com>
2024-04-26 19:25:08 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from 'expo-router';
-
+import { StripeProvider } from '@stripe/stripe-react-native';
 export default function Layout() {
-  return <Stack />;
+  return (
+    <StripeProvider publishableKey="pk_test_51NqfJ4SJIGArQAxoTt562ovvx9vyhovKlx7fwS1ETMezyklo3PMYaLX4oS8N5apJf8iGECt4G3R66GrcCqx0S3Zv00eIdT1Rl8" >
+  <Stack />
+  </StripeProvider>
+);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,79 @@
-import { Stack, Link } from 'expo-router';
+import {PlatformPay, PlatformPayButton, confirmPlatformPayPayment, usePlatformPay} from '@stripe/stripe-react-native';
+import React from 'react';
+import { Alert, View } from 'react-native';
+//npx eas build --profile development --platform android --message "stripe"
+function Home() {
+  const {
+    isPlatformPaySupported,
+    confirmPlatformPayPayment,
+  } = usePlatformPay();
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
 
-export default function Home() {
+  // React.useEffect(() => {
+  //   (async function () {
+  //     if (!(await isPlatformPaySupported({ googlePay: {testEnv: true} }))) {
+  //       Alert.alert('Google Pay is not supported.');
+  //       return;
+  //     }
+  //   })();
+  // }, []);
+
+
+  const fetchPaymentIntentClientSecret = async () => {
+    // Fetch payment intent created on the server, see above
+    const response = await fetch(`https://619d-163-53-203-209.ngrok-free.app/pay`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        currency: 'usd',
+      }),
+    });
+    const { clientSecret } = await response.json();
+
+    return clientSecret;
+  };
+
+  const pay = async () => {
+    const clientSecret = await fetchPaymentIntentClientSecret();
+
+    const { error } = await confirmPlatformPayPayment(
+      clientSecret,
+      {
+        googlePay: {
+          testEnv: true,
+          merchantName: 'test-ecommerce',
+          merchantCountryCode: 'US',
+          currencyCode: 'USD',
+          billingAddressConfig: {
+            format: PlatformPay.BillingAddressFormat.Full,
+            isPhoneNumberRequired: true,
+            isRequired: true,
+          },
+        },
+      }
+    );
+
+    if (error) {
+      Alert.alert(error.code, error.message);
+      // Update UI to prompt user to retry payment (and possibly another payment method)
+      return;
+    }
+    Alert.alert('Success', 'The payment was confirmed successfully.');
+  };
+
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <View>
+      <PlatformPayButton
+        type={PlatformPay.ButtonType.Pay}
+        onPress={pay}
+        style={{
+          width: '100%',
+          height: 50,
+        }}
+      />
+    </View>
   );
 }
+export default Home
