706610f7394b6fe47e005bfc547a1a5b1f68a34d
suyash20-code <suyashpadwal10@gmail.com>
2024-05-02 18:19:34 +0530

    charge customer

--- app/card.tsx
+++ app/card.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, Button, Alert } from 'react-native';
 import axios from 'axios';
 import { useStripe,usePaymentSheet } from '@stripe/stripe-react-native';
-
+//npx eas build --profile development --platform android --message "stripe"
 const Card = () => {
   const [cardNumber, setCardNumber] = useState('');
   const [expiry, setExpiry] = useState('');
@@ -10,6 +10,7 @@ const Card = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [ready, setReady] = useState(false);
+  const stripe = useStripe();
   const {
     initPaymentSheet,
     presentPaymentSheet,
@@ -47,7 +48,7 @@ useEffect(() => {
     }
   };
   const fetchPaymentSheetParams = async () => {
-    const response = await fetch('https://d5d6-163-53-203-209.ngrok-free.app/payment-sheet', {
+    const response = await fetch('https://5d7a-163-53-203-209.ngrok-free.app/payment-sheet', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -56,12 +57,26 @@ useEffect(() => {
     const {setupIntent, ephemeralKey, customer} = await response.json();
 
     return {
-        setupIntent,
+      setupIntent,
       ephemeralKey,
       customer,
     };
   };
+  const chargeCustomer = async () => {
+    const response = await fetch(`https://5d7a-163-53-203-209.ngrok-free.app/charge-customer?customerId=cus_Q22Uo0BTtOfUlv`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const {paymentIntent, ephemeralKey, customer} = await response.json();
 
+    return {
+      paymentIntent,
+      ephemeralKey,
+      customer,
+    };
+  };
   async function buy() {
     const {error} = await presentPaymentSheet();
 
@@ -72,6 +87,48 @@ useEffect(() => {
       setReady(false);
     }
   }
+
+  const subscribe = async () => {
+    try {
+      // sending request
+      const response = await fetch("https://5d7a-163-53-203-209.ngrok-free.app/payByCustomer", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (!response.ok) return Alert.alert(data.message);
+      const clientSecret = data.clientSecret;
+      const initSheet = await stripe.initPaymentSheet({
+        paymentIntentClientSecret: clientSecret,
+        merchantDisplayName: "suyash.com",
+        customerId: "cus_Q22Uo0BTtOfUlv",
+      
+    //   applePay: {
+    //     merchantCountryCode: 'US',
+    //   },
+    //   googlePay: {
+    //     merchantCountryCode: 'US',
+    //     testEnv: true,
+    //     currencyCode: 'usd',
+    //   },
+      allowsDelayedPaymentMethods: true,
+      returnURL: 'stripe-example://stripe-redirect',
+      });
+      if (initSheet.error) return Alert.alert(initSheet.error.message);
+      const presentSheet = await stripe.presentPaymentSheet({
+        clientSecret,
+      });
+      if (presentSheet.error) return Alert.alert(presentSheet.error.message);
+      Alert.alert("Payment complete, thank you!");
+      // router.push('/two');
+    } catch (err) {
+      console.error(err);
+      Alert.alert("Something went wrong, try again later!");
+    }
+  };
+  
 //   const handlePay = async () => {
 //     try {
 //         const response = await axios.post('https://d5d6-163-53-203-209.ngrok-free.app/create-customer');
@@ -116,6 +173,7 @@ useEffect(() => {
         placeholder="Enter email address"
       /> */}
       <Button title="Pay" onPress={buy} />
+      <Button title="charge" onPress={subscribe}></Button>
     </View>
   );
 };
